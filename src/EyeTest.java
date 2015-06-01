853180fdee402339e1ceeb6c73a7173794aa03b9
Ernest Wong <chewong@uwaterloo.ca>
2015-06-01 14:50:06 -0400

    Added streaming data feature

--- src/EyeTest.java
+++ src/EyeTest.java
@@ -1,5 +1,11 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import com.theeyetribe.client.*;
 import com.theeyetribe.client.GazeManager.*;
@@ -15,12 +21,15 @@ public class EyeTest extends JFrame {
 	private GazeListener gazeListener;
 	private JButton pause;
 	private JButton resume;
+	private JButton startStreaming;
+	private JButton stopStreaming;
 	private JTextArea[] eyeData = new JTextArea[2];
-	
+	private ArrayList<String> streamData = new ArrayList<String>();
+	private FileWriter fileWriter = null;
+
 	// Establish connection to local eye tribe server
 	private boolean isConnect = gm.activate(ApiVersion.VERSION_1_0,
 			ClientMode.PUSH);
-
 
 	private EyeTest() {
 		super("Eye Tribe Data Collector");
@@ -41,7 +50,7 @@ public class EyeTest extends JFrame {
 		}
 		eyeData[0].setBounds(10, 10, 600, 60);
 		eyeData[1].setBounds(10, 50, 600, 60);
-		
+
 		pause = new JButton("Pause Tracking");
 		pause.addActionListener(new ActionListener() {
 			@Override
@@ -51,10 +60,73 @@ public class EyeTest extends JFrame {
 		});
 		pause.setBounds(10, 90, 140, 50);
 
+		resume = new JButton("Resume Tracking");
+		resume.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				gazeListener.resume();
+			}
+		});
+		resume.setBounds(150, 90, 140, 50);
+
+		startStreaming = new JButton("Start Streaming");
+		startStreaming.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				gazeListener.startStreaming();
+				startStreaming.setText("Streaming...");
+			}
+		});
+		startStreaming.setBounds(10, 150, 140, 50);
+
+		stopStreaming = new JButton("Stop Streaming");
+		stopStreaming.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				streamData = gazeListener.stopStreaming();
+				startStreaming.setText("Start Streaming");
+				try {
+					// Check if there is data in the stream data ArrayList
+					if (streamData.size() == 0) {
+						return;
+					}
+					// Get current system time
+					String timeStamp = new SimpleDateFormat(
+							"yyyy-MM-dd_HH-mm-ss").format(Calendar
+							.getInstance().getTime());
+					File file = new File("StreamData_" + timeStamp + ".txt");
+					fileWriter = new FileWriter(file);
+
+					String temp = "";
+					temp += "Left Eye Coordinates in Pixels		Right Eye Coordinates in Pixels		Timestamp\n";
+					for (int i = 0; i < streamData.size() - 4; i = i + 3) {
+						while (streamData.get(i).equals("0, 0")
+								|| streamData.get(i + 1).equals("0, 0")) {
+							i = i + 3;
+						}
+						if (i < streamData.size() - 4) {
+							temp += String.format("%-25s%19s%41s\n",
+									streamData.get(i), streamData.get(i + 1),
+									streamData.get(i + 2));
+						}
+					}
+					fileWriter.write(temp);
+					gazeListener.stopStreaming().clear();
+				} catch (IOException exception) {
+
+				} finally {
+					try {
+						fileWriter.close();
+					} catch (IOException exception) {
+
+					}
+				}
+			}
+		});
+		stopStreaming.setBounds(150, 150, 140, 50);
+
 		if (!isConnect) {
 			eyeData[0].setText("Server Not Set Up");
-		} else {
-			eyeData[0].setText("Eye Tribe Not Calibrated");
 		}
 
 		// Add widget to screen
@@ -62,6 +134,9 @@ public class EyeTest extends JFrame {
 		this.add(eyeData[0]);
 		this.add(eyeData[1]);
 		this.add(pause);
+		this.add(resume);
+		this.add(startStreaming);
+		this.add(stopStreaming);
 
 		gazeListener = new GazeListener(eyeData);
 		gm.addGazeListener(gazeListener);
--- src/GazeListener.java
+++ src/GazeListener.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 import com.theeyetribe.client.IGazeListener;
@@ -6,7 +8,10 @@ import com.theeyetribe.client.data.GazeData;
 public class GazeListener implements IGazeListener {
 
 	private JTextArea[] eyeData;
+	private String[] eyeInfo = new String[2];
+	private ArrayList<String> streamData = new ArrayList<String>();
 	private boolean isPause = false;
+	private boolean isStream = false;;
 
 	public GazeListener(JTextArea[] eyeData) {
 		this.eyeData = eyeData;
@@ -23,30 +28,48 @@ public class GazeListener implements IGazeListener {
 		for (int i = 0; i < 2; i++) {
 			switch (i) {
 			case 0:
-				eyeData[i]
-						.setText(String
-								.format("Left Eye Coordinates in Pixels: (%.0f, %.0f)\nLeft Eye Pupil Size: %.0f mm",
-										gazeData.leftEye.smoothedCoordinates.x,
-										gazeData.leftEye.smoothedCoordinates.y,
-										gazeData.leftEye.pupilSize));
+				eyeInfo[i] = String
+						.format("Left Eye Coordinates in Pixels: (%.0f, %.0f)\nLeft Eye Pupil Size: %.0f mm",
+								gazeData.leftEye.smoothedCoordinates.x,
+								gazeData.leftEye.smoothedCoordinates.y,
+								gazeData.leftEye.pupilSize);
 				break;
 			case 1:
-				eyeData[i]
-						.setText(String
-								.format("Right Eye Coordinates in Pixels: (%.0f, %.0f)\nRight Eye Pupil Size: : %.0f mm",
-										gazeData.rightEye.smoothedCoordinates.x,
-										gazeData.rightEye.smoothedCoordinates.y,
-										gazeData.rightEye.pupilSize));
+				eyeInfo[i] = String
+						.format("Right Eye Coordinates in Pixels: (%.0f, %.0f)\nRight Eye Pupil Size: : %.0f mm",
+								gazeData.rightEye.smoothedCoordinates.x,
+								gazeData.rightEye.smoothedCoordinates.y,
+								gazeData.rightEye.pupilSize);
 				break;
 			}
 		}
+		if (isStream) {
+			streamData.add(String.format("%.0f, %.0f",
+					gazeData.leftEye.smoothedCoordinates.x,
+					gazeData.leftEye.smoothedCoordinates.y));
+			streamData.add(String.format("%.0f, %.0f",
+					gazeData.rightEye.smoothedCoordinates.x,
+					gazeData.rightEye.smoothedCoordinates.y));
+			streamData.add("" + gazeData.timeStamp);
+		}
+		eyeData[0].setText(eyeInfo[0]);
+		eyeData[1].setText(eyeInfo[1]);
 	}
 
 	public void pause() {
 		isPause = true;
 	}
-	
+
 	public void resume() {
 		isPause = false;
+	}
+
+	public void startStreaming() {
+		isStream = true;
+	}
+
+	public ArrayList<String> stopStreaming() {
+		isStream = false;
+		return streamData;
 	}
 }
